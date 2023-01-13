5188fa2b17a43795cda9d846b15365f3bc8d3136
Lyene <lyene.benvenutti@southsystem.com.br>
2023-01-13 10:00:41 -0300

    Testes unitarios da aplicacao

---
+++ src/test/java/br/ce/wcaquino/taskbackend/utils/DateUtilsTest.java
@@ -0,0 +1,28 @@
+package br.ce.wcaquino.taskbackend.utils;
+
+import org.junit.Test;
+
+import java.time.LocalDate;
+
+import static org.junit.Assert.*;
+
+public class DateUtilsTest {
+
+    @Test
+    public void deveRetornarTrueParaDatasFuturas() {
+        LocalDate date = LocalDate.of(2030, 01, 01);
+        assertTrue(DateUtils.isEqualOrFutureDate(date));
+    }
+
+    @Test
+    public void deveRetornarFalseParaDatasPassadas() {
+        LocalDate date = LocalDate.of(2010, 01, 01);
+        assertFalse(DateUtils.isEqualOrFutureDate(date));
+    }
+
+    @Test
+    public void deveRetornarTrueParaDataAtual() {
+        LocalDate date = LocalDate.now();
+        assertTrue(DateUtils.isEqualOrFutureDate(date));
+    }
+}
